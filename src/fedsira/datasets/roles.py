22c4833cd725e858466048296da6960c89bfed84
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-08-28 22:21:33 +0100

    fix: use enum keys for role intervals

--- src/fedsira/datasets/roles.py
+++ src/fedsira/datasets/roles.py
@@ -6,8 +6,8 @@ def supported_role_windows(role_intervals: RoleIntervals) -> tuple[RoleWindow, .
     return tuple(
         RoleWindow(
             role=role,
-            lower_inclusive=role_intervals.supported[role.value][0],
-            upper_exclusive=role_intervals.supported[role.value][1],
+            lower_inclusive=role_intervals.supported[role][0],
+            upper_exclusive=role_intervals.supported[role][1],
         )
         for role in SUPPORTED_ROLE_ORDER
     )
@@ -17,8 +17,8 @@ def target_role_windows(role_intervals: RoleIntervals) -> tuple[RoleWindow, ...]
     return tuple(
         RoleWindow(
             role=role,
-            lower_inclusive=role_intervals.target[role.value][0],
-            upper_exclusive=role_intervals.target[role.value][1],
+            lower_inclusive=role_intervals.target[role][0],
+            upper_exclusive=role_intervals.target[role][1],
         )
         for role in TARGET_ROLE_ORDER
     )
